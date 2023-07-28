4247a1d6acd576ffcdb4c46570452dc7fa25658f
Victoria Tumwebaze <victoria.tumwebaze@outlook.com>
2023-07-28 14:42:03 +0300

    Add views to the project

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
-# frozen_string_literal: true
-
 Rails.application.routes.draw do
   resources :users, only: %i[index show] do
     resources :posts, only: %i[index show]
+  # get 'posts/index'
   end
+  # resources :users, only: [:show]
 end
