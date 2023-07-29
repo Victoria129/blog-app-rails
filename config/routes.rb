f3ae00225032432129f41b01a4fc3a3ca8c8f139
Victoria Tumwebaze <victoria.tumwebaze@outlook.com>
2023-07-29 17:55:48 +0300

    Update forms

--- config/routes.rb
+++ config/routes.rb
@@ -1,17 +1,18 @@
+# frozen_string_literal: true
+
 Rails.application.routes.draw do
   get 'comments/new'
   get 'comments/create'
   get 'likes/new'
   get 'likes/create'
-  root "users#index"
+  root 'users#index'
 
-  resources :users, only: [:index, :show] do
-    resources :posts, only: [:index, :show, :new, :create]
+  resources :users, only: %i[index show] do
+    resources :posts, only: %i[index show new create]
   end
 
   resources :posts do
-    resources :comments, only: [:new, :create]
+    resources :comments, only: %i[new create]
     resources :likes, only: [:create]
   end
 end
-
